3c1df07845f28905ef1e172f59c8c2e2c28b15ce
Mandolin Daze <renormalize@gmail.com>
2021-06-24 22:53:02 -0700

    adjustments to README and code to increase performance

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# password-generator
+# Project Title
+
+This project is in partial fulfillment with the UC Berkeley Extension programming project --- construct a password generator using HTML, CSS, and JavaScript.
+
+So we call this project Password Generator ...
+
+## Getting Started
+
+This is a stand alone application and should work once you hit the "Generate Password" button from within the page file:///C:/Users/renor/Desktop/03-javascript/homework/Develop/index.html
+
+On that link, you can play around with how the app works. 
+
+Here is a video of me demoing the whole thing if you want to kick back and watch TV https://www.loom.com/share/0f35fe98430d41cda138aecc2857d35b
+
+### Prerequisites
+
+No prereq's to use ... but maybe a bit to evaluate?
+
+Note this user is not free to enter garbage and the code moves through ... the user either MUST enter a valid password length and valid choice selection, or they are given the option to opt out.
+
+
+
+### Installing
+
+Please let me know if you are interested in the code for this. I feature several "while" loops which serve to keep the user engaged if they really are intentional about generating a password.
+
+For the record:
+
+Requirements are:
+
+1) enter a real integer greater than or equal to 8 and less than or equal to 128. If you do not do this, my algorithm will bounce you all over the place until you enter valid input or give up. NO APOLOGIES!
+2) once agreed on a real integer length, you must specify at least one of four potential defining criteria of this alorithm's inclusion criteria. If you don't, you are sent into a loop until you either opt out, or cave! " ... go with the logic Spock ...".
+3) done? you get your password ...  
+
+
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Deployed Link
+
+* [See Live Site](#)
+
+
+## Authors
+
+* **YOUR NAME** 
+- [Link to LinkedIn](https://www.linkedin.com/in/peter-alexander-4a444127/)
+
+
+## License
+
+This project is licensed under the MIT License 
+
+## Acknowledgments
+
+* Hat tip to anyone whose code, libraries, packages, or UI was used  / inspired from
+* Inspiration
+* etc
--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //variables used to track user supplied information
 
-var passwordLength;
+var passwordLength = 0;
 var includeNumbers;
 var includeCharacters;
 var includeUppercase;
@@ -22,32 +22,58 @@ from which random charaters will be selected to form the final password as per u
 var sel;
 
 function generatePassword(){
+    /*Prompt the user for desired passwork length. Input should be an integer between 8 and 128 inclusive; this section of
+    code will test whether the user entered string data which can be parsed to a genuine integer within the above range. */
+
+        var temp = prompt("Please enter a desired length for your password; your password must be at least 8 characters in length, but no more than 128");
+        var useful = false;
+        var done = false; 
+
+        while (useful === false && done === false){
+          if ( Number(temp) && Math.abs(Number(temp) - parseInt(temp)) == 0 && parseInt(temp) > 7 && parseInt(temp) < 129){
+            passwordLength = parseInt(temp);
+            useful = true;
+          } else if (confirm("You did not input a valid password length. Do you wish to try again?")){
+            temp = prompt("Please enter a desired length for your password; your password must be at least 8 characters in length, but no more than 128");
+          } else {alert("Try again later ?"); 
+                    done = true;
+                    }
+          }
+
+        
+                             
+
     // prompt for user input
-    passwordLength = parseInt(prompt("Select password length. Choose a positive integer between 8 and 128 inclusive."));
+    /*passwordLength = parseInt(prompt("Select password length. Choose a positive integer between 8 and 128 inclusive."));
     //test for nonsense
     if (!passwordLength){
-      alert("This needs a valid input.");
+      alert("This needs a valid input.");     
     } else if (passwordLength < 8 || passwordLength > 128){
       passwordLength = parseInt(prompt("That was not a valid option. Please select an integer BETWEEN 8 and 128 inclusive."));
-    } else {
-      includeNumbers = confirm("Include digits 0 - 9 ?");
-      includeCharacters = confirm("Include non-alpha-numeric characters like $, % etc. ?");
-      includeUppercase = confirm("Include UPPER CASE letters?");
-      includeLowercase = confirm("Include lower case letters?");
-    }
+    } else if (passwordLength < 8 || passwordLength > 128) {alert("Try again")}
+    else{*/
+    
+    while (useful === true && done === false){
+    includeNumbers = confirm("Include digits 0 - 9 ?");
+    includeCharacters = confirm("Include non-alpha-numeric characters like $, % etc. ?");
+    includeUppercase = confirm("Include UPPER CASE letters?");
+    includeLowercase = confirm("Include lower case letters?");
+    
 
     //What if all the above evaluate to false?
 
     var decide = false;
 
     if(!(includeNumbers||includeCharacters||includeUppercase||includeLowercase)){
-         decide = confirm("You must enter an integer password length between 8 and 128 inclusive and select 'ok' to at least one of the choices of 'character type'. Do you wish to start again?");
-    } 
+        useful = confirm("You must enter an integer password length between 8 and 128 inclusive and select 'ok' to at least one of the choices of 'character type'. Do you wish to start again?");
+    } else {done = true}
+
+  }
 
     /*If the above confirm yields decide = true, we will repeat from line 24 and line 50 will be 
     ignored on the second run, otherwise move on to construct the string to "sample" from.*/
 
-    if(decide){generatePassword();}
+    // if(decide){generatePassword();}
 
     var sel = [includeNumbers,includeCharacters,includeUppercase,includeLowercase];
 
@@ -73,9 +99,6 @@ function generatePassword(){
     }
   return pwdSt;
 }
-
-
-
 
 
 
